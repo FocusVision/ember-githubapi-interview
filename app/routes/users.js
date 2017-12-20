8c81997367332eb9127473e6d3f05e82ef96c6ca
ksure <kyle.scherling@gmail.com>
2017-12-19 18:04:42 -0800

    Adds users route, move application there

---
+++ app/routes/users.js
@@ -0,0 +1,18 @@
+import Ember from 'ember'
+
+export default Ember.Route.extend({
+  queryParams: {
+    q: {
+      as: 'query',
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    if (params && params.q) {
+      return this.get('store').query('user', params)
+    }
+
+    return []
+  }
+})
